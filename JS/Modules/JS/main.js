eac5df221fc9620a6a52bdf4c14dd8e711bd8396
James Morgan O <MorganJay@users.noreply.github.com>
2020-10-31 16:20:27 +0100

    Modules & JSON

---
+++ JS/Modules/JS/main.js
@@ -0,0 +1,11 @@
+// import { square as utilsSquare, name } from './utils.js';
+// import * as Utils from './utils.js'; 
+// import {default as bottle} from './utils.js';
+import bottle from './utils.js';
+
+import { water}  from './utils.js';
+
+bottle(); 
+water();
+// console.log(Utils.square(4));
+// console.log(Utils.name );
---
+++ JS/Modules/JS/utils.js
@@ -0,0 +1,13 @@
+// function square(n){
+//     return n ** 2;
+// }
+
+// const name = 'Dom';
+
+// export{ name, square };
+
+export default function sayName(){
+    console.log("Dom!");
+}
+
+export {water} from './helper.js';
